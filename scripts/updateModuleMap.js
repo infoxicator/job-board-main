d07450e955b36d05567065444af84346740f28d3
infoxicator <ruben@infoxication.net>
2020-02-25 00:07:28 +0000

    feat(travis): s3 modulemap

--- scripts/updateModuleMap.js
+++ scripts/updateModuleMap.js
@@ -1,37 +1,48 @@
 const fs = require('fs-extra');
 const uuidv4 = require('uuid/v4');
+const fetch = require('node-fetch');
 const { name, version } = require('../package.json');
 
-const getUrlFromCNAME = async () => {
-  let domainUrl = await fs.readFile('./CNAME', 'utf8');
-  domainUrl = domainUrl.replace(/(\r\n|\n|\r)/gm, '');
-  return `https://${domainUrl}/`;
-};
+// const getUrlFromCNAME = async () => {
+//   let domainUrl = await fs.readFile('./CNAME', 'utf8');
+//   domainUrl = domainUrl.replace(/(\r\n|\n|\r)/gm, '');
+//   return `https://${domainUrl}/`;
+// };
 
 const integrityDigests = require('../bundle.integrity.manifest.json');
 
+const moduleMapUrl = 'https://one-app-modules.s3.eu-west-2.amazonaws.com/module-map.json';
+
 const doWork = async () => {
-  const moduleMapContent = { key: uuidv4(), modules: {} };
-  const STATIC_ASSETS_URL = await getUrlFromCNAME();
+  try {
+    const response = await fetch(moduleMapUrl);
+    const moduleMapContent = await response.json();
+    // const moduleMapContent = { key: uuidv4(), modules: {} };
+    // const STATIC_ASSETS_URL = await getUrlFromCNAME();
+    const STATIC_ASSETS_URL = 'https://one-app-modules.s3.eu-west-2.amazonaws.com/modules';
+    moduleMapContent.key = uuidv4();
 
-  moduleMapContent.modules[name] = {
-    browser: {
-      url: `${STATIC_ASSETS_URL}${version}/${name}.browser.js`,
-      integrity: integrityDigests.browser,
-    },
-    legacyBrowser: {
-      url: `${STATIC_ASSETS_URL}${version}/${name}.legacy.browser.js`,
-      integrity: integrityDigests.legacyBrowser,
-    },
-    node: {
-      url: `${STATIC_ASSETS_URL}${name}/${version}/${name}.node.js`,
-      integrity: integrityDigests.node,
-    },
-  };
+    moduleMapContent.modules[name] = {
+      browser: {
+        url: `${STATIC_ASSETS_URL}/${name}/${version}/${name}.browser.js`,
+        integrity: integrityDigests.browser,
+      },
+      legacyBrowser: {
+        url: `${STATIC_ASSETS_URL}/${name}/${version}/${name}.legacy.browser.js`,
+        integrity: integrityDigests.legacyBrowser,
+      },
+      node: {
+        url: `${STATIC_ASSETS_URL}/${name}/${version}/${name}.node.js`,
+        integrity: integrityDigests.node,
+      },
+    };
 
-  await fs.writeFile(
-    './map/module.map.json', JSON.stringify(moduleMapContent, null, 2)
-  );
+    await fs.writeFile(
+      './map/module.map.json', JSON.stringify(moduleMapContent, null, 2)
+    );
+  } catch (e) {
+    console.log(e);
+  }
 };
 
 doWork().catch((err) => {
